0ecd37d0c0ce8786e9f39bc1cd590ca4010d3a90
dezegon <gonzaloediaz@gmail.com>
2020-08-11 20:27:56 -0300

    customerList and customerTile lws added

---
+++ es-space-mgmt/main/default/lwc/customerTile/customerTile.js
@@ -0,0 +1,18 @@
+import { LightningElement, api } from 'lwc';
+
+export default class CustomerTile extends LightningElement {
+
+    @api customer;
+    @api object;
+
+    connectedCallBack() {
+    }
+
+    get icon() {
+        return 'doctype:image';
+    }
+
+    get alttext() {
+        return 'image';
+    }
+}
